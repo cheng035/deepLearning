cf70899353785cf8988e740f508f0dd8af3a7a55
Cheng Haohan <haohan.cheng@shopee.com>
2024-09-09 15:36:30 +0800

    implement a simple perception machine to realise nand, and, or functions

--- 01-numpy.py
+++ 01-numpy.py
@@ -2,18 +2,35 @@ import numpy as np
 
 
 # the function of Perceptron
-def perceptron(x1, x2, w1, w2, weight):
-    input = np.array(x1, x2)
-    weight = np.array(w1, w2)
+def perceptron(x1, x2, w1, w2, bias):
+    input = np.array([x1, x2])
+    weight = np.array([w1, w2])
     result = sum(input * weight)
-    if result + weight > 0:
+    if result + bias > 0:
         return True
     else:
         return False
 
 
 def orGate(x1, x2):
-    return perceptron(x1, x2, 1, 1, 0.1)
+    return perceptron(x1, x2, 1, 1, -0.1)
 
 
-print(orGate(0, 0))
+def nandGate(x1, x2):
+    return perceptron(x1, x2, -0.5, -0.5, 0.7)
+
+
+def andGate(x1, x2):
+    return perceptron(x1, x2, 0.5, 0.5, -0.7)
+
+
+def xorGate(x1, x2):
+    s1 = nandGate(x1, x2)
+    s2 = orGate(x1, x2)
+    y = andGate(s1, s2)
+    return y
+
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        print(i, j, xorGate(i, j))
